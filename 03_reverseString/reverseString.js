7c50c3e8431863fa14131a1291b96e6b52315d56
Vivi <lenardvivien@yahoo.de>
2024-09-24 22:57:27 +0200

    first two solved. the third one is in c style. need to research easier functions.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let tmp = '';
+    let n = 0;
+    console.log(string.length)
+    for (let i = string.length - 1; i >= 0; i--){
+        tmp += string[i]
+        n++
+    }
+    return tmp
 };
 
+console.log(reverseString('hi'))
 // Do not edit below this line
 module.exports = reverseString;
